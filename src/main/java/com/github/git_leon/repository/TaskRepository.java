78e35797d4226546c9b7d3963126badec3d72b46
leon hunter <xleonhunter@gmail.com>
2020-03-11 18:09:35 -0400

    emptied repo

--- src/main/java/com/github/git_leon/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.git_leon.repository;
-
-import com.github.git_leon.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
--- src/main/java/com/github/git_leon/utils/EntityInterface.java
+++
@@ -1,7 +0,0 @@
-package com.github.git_leon.utils;
-
-import java.io.Serializable;
-
-public interface EntityInterface<IdType extends Serializable> {
-    IdType getId();
-}
