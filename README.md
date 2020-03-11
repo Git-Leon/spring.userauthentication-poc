7180cfabc166ace7e4d30d1caadc23e713b3a45a
leon hunter <xleonhunter@gmail.com>
2020-03-11 18:22:25 -0400

    created readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Okta User Auth Implementation
+* **Objective**
+    * To implement
+* **Purpose**
+    * To demonstrate
+* **Description**
+    * This application
+
+_This application is based on a tutorial which can be found [here](https://developer.okta.com/blog/2018/10/05/build-a-spring-boot-app-with-user-authentication)._
--- src/main/java/com/okta/springbootwithauth/SpringSecurityWebAppConfig.java
+++ src/main/java/com/okta/springbootwithauth/SpringSecurityWebAppConfig.java
@@ -1,8 +1,6 @@
 package com.okta.springbootwithauth;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
